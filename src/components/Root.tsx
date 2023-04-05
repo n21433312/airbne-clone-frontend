c020c2886569f6e1d559f64dd23edddda2d61f7a
n21433312 <n21433312@gmail.com>
2023-04-06 03:06:15 +0900

    헤더 만들기

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,7 +1,24 @@
+import { Box, HStack, Button } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
 export default function Root() {
-    return <h1>im root
-        <Outlet />
-            </h1>    
+    return (
+        <Box>
+            <HStack 
+              justifyContent={"space-between"} 
+              py={5} px={10} 
+              borderBottomWidth={1}
+            >
+              <Box color="red.500" >
+                <FaAirbnb size={"48"} />
+              </Box>
+              <HStack spacing={2}>
+                  <Button>Log in</Button>
+                  <Button colorScheme={"red"}>Sign in</Button>
+              </HStack>
+            </HStack>
+            <Outlet />
+        </Box>    
+    );
 }
