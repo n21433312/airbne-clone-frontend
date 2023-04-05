754812590c1f65cc60e8d54c86bdbd62915ae4d4
n21433312 <n21433312@gmail.com>
2023-04-06 02:37:25 +0900

    차크라를 이용하여 페이지 만들기

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
-import Users from "./routes/Users";
 import Home from "./routes/Home";
+import NotFound from "./routes/NotFound";
 
 const router =createBrowserRouter([{
     path: "/", 
     element: <Root />, 
+    errorElement: <NotFound />, 
     children: [
         {
             path:"",
             element:<Home />
-        },
-        {
-            path:"users",
-            element:<Users />
         },
     ]
 }])
---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,14 @@
+import { VStack, Heading, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+
+
+export default function NotFound(){
+    return <VStack bg="gray.100" justifyContent={"center"} minH="100vh">
+        <Heading>Page not found.</Heading>
+        <Text>It seems that you're lost.</Text>
+        <Link to="/">
+            <Button colorScheme={"twitter"} variant={"link"}>Go home &rarr;</Button>
+        </Link>
+    </VStack>;
+}
